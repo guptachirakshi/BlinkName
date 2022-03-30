f4b5233891ca8d22c94a48dffa1084d373a1e937
guptachirakshi <96788216+guptachirakshi@users.noreply.github.com>
2022-03-30 08:15:52 +0530

    Add files via upload

---
+++ morse-code.ino
@@ -0,0 +1,151 @@
+void setup() {
+    pinMode(D7, OUTPUT);
+}
+
+void loop() {
+    //C
+    digitalWrite(D7, HIGH);
+    delay(1500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(1500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    
+    //Delay 1.5s before next alphabet
+    delay(1500);
+    
+    //H
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    
+    //Delay 1.5s before next alphabet
+    delay(1500);
+    
+    //I
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    
+    //Delay 1.5s before next alphabet
+    delay(1500);
+    
+    //R
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(1500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    
+    //Delay 1.5s before next alphabet
+    delay(1500);
+    
+    //A
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(1500);
+    digitalWrite(D7, LOW);
+    
+    //Delay 1.5s before next alphabet
+    delay(1500);
+    
+    //K
+    digitalWrite(D7, HIGH);
+    delay(1500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(1500);
+    digitalWrite(D7, LOW);
+    
+    //Delay 1.5s before next alphabet
+    delay(1500);
+    
+    //S
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    
+    //Delay 1.5s before next alphabet
+    delay(1500);
+    
+    //H
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    
+    //Delay 1.5s before next alphabet
+    delay(1500);
+    
+    //I
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    delay(500);
+    digitalWrite(D7, HIGH);
+    delay(500);
+    digitalWrite(D7, LOW);
+    
+    //Delay 3.5s before re-looping
+    delay(3500);
+    
+}
